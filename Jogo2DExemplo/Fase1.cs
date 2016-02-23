b0a302e8b5ca2321165879e1fa226f6c4ab325cc
Francisco Calaça Xavier <chicocx@gmail.com>
2016-02-23 09:47:53 -0300

    comite inicial

---
+++ Jogo2DExemplo/Fase1.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Jogo2DExemplo
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Fase1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        private Texture2D background;
+        private Texture2D shuttle;
+
+        private Personagem jogador = new Personagem(150, 240);
+        private Personagem inimigo = new Personagem(450, 240);
+
+        public Fase1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            background = Content.Load<Texture2D>("stars"); // change these names to the names of your images
+            shuttle = Content.Load<Texture2D>("shuttle");  // if you are using your own images.
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            Keys[] keys = Keyboard.GetState().GetPressedKeys();
+            foreach(Keys k in keys)
+            {
+                if (k.Equals(Keys.Up))
+                {
+                    jogador.moverY(-2);
+                }
+                if (k.Equals(Keys.Down))
+                {
+                    jogador.moverY(2);
+                }
+                if (k.Equals(Keys.Right))
+                {
+                    jogador.moverX(2);
+                }
+                if (k.Equals(Keys.Left))
+                {
+                    jogador.moverX(-2);
+                }
+            }
+
+            // TODO: Add your update logic here
+
+
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+            spriteBatch.Draw(background, new Rectangle(0, 0, 800, 480), Color.White);
+            spriteBatch.Draw(shuttle, jogador.getVector(), Color.White);
+            spriteBatch.End();
+
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Jogo2DExemplo/Personagem.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Jogo2DExemplo
+{
+    class Personagem
+    {
+        public int x;
+
+        public int y;
+
+        public Personagem(int x1, int y1)
+        {
+            x = x1;
+            y = y1;
+        }
+
+        public Vector2 getVector()
+        {
+            return new Vector2(x, y);
+        }
+
+        public void moverX(int qtdPassos)
+        {
+            x += qtdPassos;
+        }
+
+        public void moverY(int qtdPassos)
+        {
+            y += qtdPassos;
+        }
+
+    }
+}
